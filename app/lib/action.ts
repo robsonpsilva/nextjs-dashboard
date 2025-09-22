25a205664fac45a021eacb93ba4519549edfd07d
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-09-22 16:50:46 -0300

    modified:   app/lib/action.ts 	modified:   app/lib/action.ts

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,5 +1,9 @@
 'use server';
 import {z} from 'zod';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -15,6 +19,12 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 }
  
 // export async function createInvoice(formData: FormData) {
